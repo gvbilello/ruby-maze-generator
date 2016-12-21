0c586150cd48427a6d29f9aa5ce0088e48298c3c
Gregory V. Bilello <gregory.bilello@gmail.com>
2016-12-21 14:59:56 -0500

    Moved check_neighbors method to Dungeon class, Cell objects no longer contain @dungeon data.

--- array.rb
+++ array.rb
@@ -7,67 +7,11 @@ class Cell
   attr_accessor :visited
 
   def initialize(args = {})
-    @dungeon = args.fetch(:dungeon)
     @x = args.fetch(:x)
     @y = args.fetch(:y)
     # @walls order: top, right, bottom, left
     @walls = [true, true, true, true]
     @visited = false
-  end
-
-  def check_neighbors
-    neighbors = []
-    top = nil
-    right = nil
-    bottom = nil
-    left = nil
-
-    if @y > 0
-      top = @dungeon.dungeon[@y - 1][@x]
-    end
-
-    if @x > 0
-      left = @dungeon.dungeon[@y][@x - 1]
-    end
-
-    if @x < @dungeon.width - 1
-      right = @dungeon.dungeon[@y][@x + 1]
-    end
-
-    if @y < @dungeon.height - 1
-      bottom = @dungeon.dungeon[@y + 1][@x]
-    end
-
-    # binding.pry
-    if top
-      if !top.visited
-        neighbors << top
-      end
-    end
-
-    if right
-      if !right.visited
-        neighbors << right
-      end
-    end
-
-    if bottom
-      if !bottom.visited
-        neighbors << bottom
-      end
-    end
-
-    if left
-      if !left.visited
-        neighbors << left
-      end
-    end
-
-    if neighbors.length > 0
-      return neighbors.sample
-    else
-      return nil
-    end
   end
 
 end
@@ -90,30 +34,77 @@ class Dungeon
     (1..@height).each do |row|
       new_row = []
       (1..@width).each do |col|
-        new_row << Cell.new(dungeon: self, x: col - 1, y: row - 1)
+        new_row << Cell.new(x: col - 1, y: row - 1)
       end
       @dungeon << new_row
     end
+  end
 
+  def check_neighbors(cell)
+    neighbors = []
+    top = nil
+    right = nil
+    bottom = nil
+    left = nil
+
+    if cell.y > 0
+      top = @dungeon[cell.y - 1][cell.x]
+    end
+    if cell.x > 0
+      left = @dungeon[cell.y][cell.x - 1]
+    end
+    if cell.x < @width - 1
+      right = @dungeon[cell.y][cell.x + 1]
+    end
+    if cell.y < @height - 1
+      bottom = @dungeon[cell.y + 1][cell.x]
+    end
+
+    if top
+      if !top.visited
+        neighbors << top
+      end
+    end
+    if right
+      if !right.visited
+        neighbors << right
+      end
+    end
+    if bottom
+      if !bottom.visited
+        neighbors << bottom
+      end
+    end
+    if left
+      if !left.visited
+        neighbors << left
+      end
+    end
+
+    if neighbors.length > 0
+      return neighbors.sample
+    else
+      return nil
+    end
   end
 
   def create_doors
     # start in top left corner
     @current = @dungeon[0][0]
-    p "y:#{current.y}, x:#{@current.x}"
+    # p "y:#{current.y}, x:#{@current.x}"
     @current.visited = true
     @visited_cells << @current
 
     until @visited_cells.length == @num_of_cells
       # binding.pry
-      next_cell = @current.check_neighbors
+      next_cell = check_neighbors(@current)
 
       if next_cell
         next_cell.visited = true
         @stack << @current
         remove_walls(@current, next_cell)
         @current = next_cell
-        p "y:#{current.y}, x:#{@current.x}"
+        # p "y:#{current.y}, x:#{@current.x}"
         @visited_cells << @current
       else
         @current = @stack.pop
@@ -128,7 +119,7 @@ class Dungeon
       next_cell.walls[1] = false
     elsif dx == -1
       current_cell.walls[1] = false
-      next_cell.walls[1] = false
+      next_cell.walls[3] = false
     end
 
     dy = current_cell.y - next_cell.y
@@ -144,15 +135,16 @@ class Dungeon
   def show_dungeon
     @dungeon.each do |row|
       row.each do |cell|
-        p cell.visited
-        p cell.walls
+        p cell
       end
     end
   end
 
 end
 
-dungeon = Dungeon.new(height: 3, width: 3)
+# create 4x4 dungeon
+dungeon = Dungeon.new(height: 4, width: 4)
 dungeon.create_dungeon
+# create maze by removing walls
 dungeon.create_doors
 dungeon.show_dungeon
